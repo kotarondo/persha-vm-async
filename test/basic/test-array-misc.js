792db55aaecce88152e28c04c2fd622cc7fecec2
Kotaro Endo <kotarondo@gmail.com>
2022-01-04 12:45:23 +0000

    .

--- test/basic/test-array-misc.js
+++ test/basic/test-array-misc.js
@@ -81,6 +81,13 @@ a.__proto__ = p;
 a[2] = 3;
 results.push([a.length, a[2]]);
 
+var a = [15];
+var p = [25, , 27, 28];
+a.__proto__ = p;
+Object.preventExtensions(a);
+a[1] = 3;
+results.push([a.length, a[1]]);
+
 throw [results, [
     '[object String]',
     '',
@@ -102,4 +109,5 @@ throw [results, [
     [5, 6, 7],
     true,
     [1, 27],
+    [1, undefined],
 ], "DONE"];
