3e9da26a892e1fb88f13b484b87002b0ac56f9ae
Kotaro Endo <kotarondo@gmail.com>
2022-01-05 14:11:26 +0000

    .

---
+++ test/basic/test-error-stack-trace.js
@@ -0,0 +1,17 @@
+// Copyright (c) 2022, Kotaro Endo.
+// All rights reserved.
+// License: "BSD-3-Clause"
+
+var results = [];
+
+(function fn1(){
+    var e = new Error('xyz');
+    results.push(e.stack);
+})();
+
+
+throw [results, [
+   'Error: xyz\n' +
+     '    at fn1 (test-error-stack-trace.js:8:11)\n' +
+     '    at test-error-stack-trace.js:7:1'
+], "DONE"];
